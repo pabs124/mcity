2f6b01b94ae244c9c884fb1b2ff9fef4678ab270
Sahil Pabby <pabs124@gmail.com>
2018-12-21 06:32:48 -0500

    header and footer finished

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './Hoc/Layout'
+
+const Routes = (props) => {
+  return (
+    <Layout>
+      second lines
+    </Layout>
+  )
+}
+
+export default Routes
